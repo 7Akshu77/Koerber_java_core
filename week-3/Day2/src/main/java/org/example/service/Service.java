11fc33e7b91e7796f67fe18e731f4f5c1cc37d33
Akshaya S <akshaya.sharavana@koerber-supplychain.com>
2025-01-27 00:23:30 +0530

    finally done

---
+++ week-3/Day2/src/main/java/org/example/service/Service.java
@@ -0,0 +1,14 @@
+package org.example.service;
+
+import org.example.persistence.Book;
+
+import java.util.List;
+
+public interface Service {
+    public List<Book> getAllBooks();
+    public Book addBook(Book book);
+    public void deleteBook(int id);
+    public void updateBook(int id, Book book);
+    public Book getBookById(int id);
+
+}

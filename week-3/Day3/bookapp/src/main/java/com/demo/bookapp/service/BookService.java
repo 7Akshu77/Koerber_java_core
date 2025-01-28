983bf5ecdfda9b7625ae8109a9f54fbe830ae626
Akshaya S <akshaya.sharavana@koerber-supplychain.com>
2025-01-28 21:18:54 +0530

    implemented REST endpoint in mysql and verified using postman

---
+++ week-3/Day3/bookapp/src/main/java/com/demo/bookapp/service/BookService.java
@@ -0,0 +1,19 @@
+package com.demo.bookapp.service;
+
+import com.demo.bookapp.entities.Book;
+
+import java.util.List;
+
+public interface BookService {
+    public List<Book> getAllBooks();
+
+    public Book getBookById(int id) throws Throwable;
+
+    public Book addBook(Book book);
+
+    public Book updateBook(int id,Book book);
+
+    public void deleteBookById(int id);
+
+    public Book getByCategory(String category);
+}
